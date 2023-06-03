be1f9481d7f09c2ac527eb1a16232cddf470d361
akash <pakash917@gmail.com>
2023-06-03 17:09:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const https = require("https");
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+const blog1 = 'Bankai refers to a Zanpakutō second release, and it can usually only be achieved by Captain-level Soul Reapers. Fans have seen more than a dozen of these Bankai, and they are all extremely powerful, but some are far stronger than others.';
+const q = 'will be posting everything i am going through';
+
+let posts = []; 
+
+
+
+app.get('/', function(req, res){
+    res.render('home');
+    console.log(posts);
+})
+
+app.get('/contact', function(req, res){
+    res.render('contact');
+})
+
+app.get('/about', function(req, res){
+    res.render('about');
+})
+
+app.get('/compose', function(req, res){
+    res.render('compose');
+})
+app.post('/', function(req, res){
+    const blog1={
+        blog_title: req.body.blogtitle,
+        blog: req.body.blogcontent
+    };
+
+    posts.push(blog1);
+    res.redirect("/");
+})
+
+app.listen( 3000,function(){
+    
+    console.log("server is running ");
+
+
+
+});
